772408e39f4ea13a46bdb347c326b4b2acf528d0
MxFxM <mxfxmmueller@gmail.com>
2020-05-05 12:29:38 +0100

    working

--- forecast.py
+++ forecast.py
@@ -1,29 +1,72 @@
 import configparser
 import requests
 import sys
-#from influxdb import InfluxDBClient
-import json
+from influxdb import InfluxDBClient
  
 def get_api_key():
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config['openweathermap']['api']
+
+def get_limits():
+    limits = configparser.ConfigParser()
+    limits.read('limits.ini')
+    return limits['ForecastLimits']
  
 def get_forecast(api_key, location):
     url = "https://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid={}".format(location, api_key)
     r = requests.get(url)
     return r.json()
 
-def get_body(weather):
+def approve(date):
+    temperature = float(date['main']['temp'])
+    pressure = float(date['main']['pressure'])
+    humidity = float(date['main']['humidity'])
+    windspeed = float(date['wind']['speed'])
+    clouds = float(date['clouds']['all'])
+
+    limits = get_limits()
+
+    approved = True
+
+    if temperature < float(limits['TemperatureMinimum']):
+        approved = False
+    if temperature > float(limits['TemperatureMaximum']):
+        approved = False
+
+    if pressure < float(limits['PressureMinimum']):
+        approved = False
+    if pressure > float(limits['PressureMaximum']):
+        approved = False
+
+    if humidity < float(limits['HumidityMinimum']):
+        approved = False
+    if humidity > float(limits['HumidityMaximum']):
+        approved = False
+
+    if windspeed < float(limits['WindspeedMinimum']):
+        approved = False
+    if windspeed > float(limits['WindspeedMaximum']):
+        approved = False
+
+    if clouds < float(limits['CloudsMinimum']):
+        approved = False
+    if clouds > float(limits['CloudsMaximum']):
+        approved = False
+
+    return approved
+
+def get_body(date):
     json_body = [{"measurement": "Oberderdingen",
                   "fields": {
-                      "Temperature": weather['main']['temp'],
-                      "Pressure": weather['main']['pressure'],
-                      "Humidity": weather['main']['humidity'],
-                      "Windspeed": weather['wind']['speed'],
-                      "Winddirection": weather['wind']['deg'],
-                      "Clouds": weather['clouds']['all']
-                      }
+                      "Temperature": float(date['main']['temp']),
+                      "Pressure": int(date['main']['pressure']),
+                      "Humidity": int(date['main']['humidity']),
+                      "Windspeed": float(date['wind']['speed']),
+                      "Clouds": int(date['clouds']['all']),
+                      "Approved": approve(date)
+                      },
+                  "time": date['dt_txt']
                 }]
     return json_body
  
@@ -37,15 +80,12 @@ def main():
  
     api_key = get_api_key()
     forecast = get_forecast(api_key, location)
- 
-    print(forecast)
 
-    with open("forecast.json", "w+") as f:
-        json.dump(forecast, f)
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'FORECAST')
+ 
+    for date in forecast['list']:
+        client.write_points(get_body(date))
 
-#    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'WEATHER')
-#    client.write_points(get_body(weather))
- 
- 
+
 if __name__ == '__main__':
     main()
